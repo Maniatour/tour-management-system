d5be82b5bc6ea80447dd80b760e93dc9347ba898
chad <info@maniaotur.com>
2025-10-09 01:14:08 -0700

    webpack 청크 분할 최적화로 런타임 에러 해결
    - Next.js 설정에 webpack 청크 분할 최적화 추가
    - 청크 파일 누락 문제 해결
    - 빌드 캐시 정리로 안정성 개선

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,28 @@ const nextConfig = {
 	env: {
 		NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
 		NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID: process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID,
+	},
+	webpack: (config: any, { isServer }: { isServer: boolean }) => {
+		// 청크 분할 최적화
+		if (!isServer) {
+			config.optimization.splitChunks = {
+				chunks: 'all',
+				cacheGroups: {
+					default: {
+						minChunks: 2,
+						priority: -20,
+						reuseExistingChunk: true,
+					},
+					vendor: {
+						test: /[\\/]node_modules[\\/]/,
+						name: 'vendors',
+						priority: -10,
+						chunks: 'all',
+					},
+				},
+			}
+		}
+		return config
 	}
 }
 
